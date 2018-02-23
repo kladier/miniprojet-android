761e7183717b87ee6e53e87d7eda07d6acc38012
kladier <kevin.ladier@gmail.com>
2018-02-23 10:56:16 +0100

    Initializing MainActivity and links between activities

--- app/src/main/java/univ/pr/nj/keewitz/MainActivity.java
+++ app/src/main/java/univ/pr/nj/keewitz/MainActivity.java
@@ -1,7 +1,9 @@
 package univ.pr.nj.keewitz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onMapButtonClick(View view) {
+        startActivity(new Intent(this, MapActivity.class));
+    }
+
+    public void onTimeTableButtonClick(View view) {
+        startActivity(new Intent(this, TimeTableActivity.class));
+    }
+
+    public void onSettingsButtonClick(View view) {
+        startActivity(new Intent(this, SettingsActivity.class));
     }
 }
