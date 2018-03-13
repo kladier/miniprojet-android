1b10e4444915d093615cdd6ceacb868bf0cda188
Alessio Coltellacci <lightplay8@gmail.com>
2018-03-14 00:07:05 +0100

    Refactor MainActivity following the refactor of his layout

--- app/src/main/java/univ/pr/nj/keewitz/MainActivity.java
+++ app/src/main/java/univ/pr/nj/keewitz/MainActivity.java
@@ -3,30 +3,48 @@ package univ.pr.nj.keewitz;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.util.Log;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    CardView map, settings, timetable, qrcode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+
+        map = findViewById(R.id.map);
+        settings = findViewById(R.id.settings);
+        timetable = findViewById(R.id.time_table);
+        qrcode = findViewById(R.id.qrcode);
+
+        map.setOnClickListener(this);
+        settings.setOnClickListener(this);
+        timetable.setOnClickListener(this);
+        qrcode.setOnClickListener(this);
     }
 
-    public void onMapButtonClick(View view) {
-        startActivity(new Intent(this, MapActivity.class));
-    }
-
-    public void onTimeTableButtonClick(View view) {
-        startActivity(new Intent(this, TimeTableActivity.class));
-    }
-
-    public void onSettingsButtonClick(View view) {
-        startActivity(new Intent(this, SettingsActivity.class));
-    }
-
-    public void onScanQRCode(View view) {
-        startActivity(new Intent(this, ScanQRCodeActivity.class));
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.map:
+                startActivity(new Intent(this, MapActivity.class));
+                break;
+            case R.id.settings:
+                startActivity(new Intent(this, SettingsActivity.class));
+                break;
+            case R.id.time_table:
+                startActivity(new Intent(this, TimeTableActivity.class));
+                break;
+            case R.id.qrcode:
+                startActivity(new Intent(this, ScanQRCodeActivity.class));
+                break;
+            default:
+                Log.d(this.getClass().getCanonicalName(), "You forgot to set the startActivity binding with your new button");
+        }
     }
 }
