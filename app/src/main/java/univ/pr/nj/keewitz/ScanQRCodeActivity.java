48face8188902083ae81c4000d8fcebdec60e860
kladier <kevin.ladier@gmail.com>
2018-03-15 18:09:14 +0100

    make qr code detector works

--- app/src/main/java/univ/pr/nj/keewitz/ScanQRCodeActivity.java
+++ app/src/main/java/univ/pr/nj/keewitz/ScanQRCodeActivity.java
@@ -1,125 +1,192 @@
 package univ.pr.nj.keewitz;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
-import android.support.v4.app.NavUtils;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.util.SparseArray;
-import android.view.MenuItem;
-import android.webkit.WebView;
-import android.widget.ImageView;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.vision.Frame;
 import com.google.android.gms.vision.barcode.Barcode;
 import com.google.android.gms.vision.barcode.BarcodeDetector;
 
 import java.io.File;
-
-/**
- * Created by what on 08/03/18.
- */
+import java.io.FileNotFoundException;
 
 public class ScanQRCodeActivity extends AppCompatActivity {
-
-    private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+    private static final String LOG_TAG = "Barcode Scanner API";
+    private static final int PHOTO_REQUEST = 10;
+    private TextView scanResults;
+    private BarcodeDetector detector;
     private Uri imageUri;
+    private static final int REQUEST_WRITE_PERMISSION = 20;
+    private static final String SAVED_INSTANCE_URI = "uri";
+    private static final String SAVED_INSTANCE_RESULT = "result";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan_qrcode);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-        this.takePhoto();
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            // Respond to the action bar's Up/Home button
-            case android.R.id.home:
-                NavUtils.navigateUpFromSameTask(this);
-                return true;
+        Button button = (Button) findViewById(R.id.button);
+        scanResults = (TextView) findViewById(R.id.scan_results);
+        if (savedInstanceState != null) {
+            imageUri = Uri.parse(savedInstanceState.getString(SAVED_INSTANCE_URI));
+            scanResults.setText(savedInstanceState.getString(SAVED_INSTANCE_RESULT));
         }
-        return super.onOptionsItemSelected(item);
-    }
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ActivityCompat.requestPermissions(ScanQRCodeActivity.this, new
+                        String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_WRITE_PERMISSION);
+            }
+        });
 
-    public void takePhoto() {
-        final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
-
-        //Création d'un intent
-        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
-
-        //Création du fichier image
-        File photo = new File(Environment.getExternalStorageDirectory(),  "Pic.jpg");
-        intent.putExtra(MediaStore.EXTRA_OUTPUT,
-                Uri.fromFile(photo));
-        imageUri = Uri.fromFile(photo);
-
-        //On lance l'intent
-        startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
-
+        detector = new BarcodeDetector.Builder(getApplicationContext())
+                .setBarcodeFormats(Barcode.DATA_MATRIX | Barcode.QR_CODE)
+                .build();
+        if (!detector.isOperational()) {
+            scanResults.setText("Could not set up the detector!");
+            return;
+        }
     }
 
     @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
-                Bitmap qrCodeBitmap = BitmapFactory.decodeResource(
-                        getApplicationContext().getResources(),
-                        R.drawable.example_qrcode_1);
-
-                BarcodeDetector detector = new BarcodeDetector.Builder(getApplicationContext())
-                                .setBarcodeFormats(Barcode.DATA_MATRIX | Barcode.QR_CODE)
-                                .build();
-                if(!detector.isOperational()){
-                    displayTxtView("Could not set up the detector!");
-                    return;
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQUEST_WRITE_PERMISSION:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    takePicture();
+                } else {
+                    Toast.makeText(ScanQRCodeActivity.this, "Permission Denied!", Toast.LENGTH_SHORT).show();
                 }
-
-                String urlFromCode = getUrlFromCode(detector, qrCodeBitmap);
-
-                displayQRCodeImg(qrCodeBitmap);
-                displayTxtView(urlFromCode);
-                displayPdfInFrame(urlFromCode);
         }
     }
 
-    private void displayTxtView(String txt) {
-        TextView txtView = findViewById(R.id.qrcode);
-        txtView.setText(txt);
-    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PHOTO_REQUEST && resultCode == RESULT_OK) {
+            launchMediaScanIntent();
+            try {
+                Bitmap bitmap = decodeBitmapUri(this, imageUri);
+                if (detector.isOperational() && bitmap != null) {
+                    Frame frame = new Frame.Builder().setBitmap(bitmap).build();
+                    SparseArray<Barcode> barcodes = detector.detect(frame);
+                    for (int index = 0; index < barcodes.size(); index++) {
+                        Barcode code = barcodes.valueAt(index);
+                        scanResults.setText(scanResults.getText() + code.displayValue + "\n");
 
-    private void displayQRCodeImg(Bitmap qrCodeBitmap) {
-        ImageView myImageView = findViewById(R.id.imageView);
-        myImageView.setImageBitmap(qrCodeBitmap);
-    }
-
-    private void displayPdfInFrame(String url) {
-        WebView wb = findViewById(R.id.pdf_webview);
-        wb.setBackgroundColor(0x00000000);
-        wb.getSettings().setJavaScriptEnabled(true);
-
-        if (url == null || url.isEmpty()) {
-            Log.w("ScanQRCodeActivity", "Error URL empty or null");
+                        //Required only if you need to extract the type of barcode
+                        int type = barcodes.valueAt(index).valueFormat;
+                        switch (type) {
+                            case Barcode.CONTACT_INFO:
+                                Log.i(LOG_TAG, code.contactInfo.title);
+                                break;
+                            case Barcode.EMAIL:
+                                Log.i(LOG_TAG, code.email.address);
+                                break;
+                            case Barcode.ISBN:
+                                Log.i(LOG_TAG, code.rawValue);
+                                break;
+                            case Barcode.PHONE:
+                                Log.i(LOG_TAG, code.phone.number);
+                                break;
+                            case Barcode.PRODUCT:
+                                Log.i(LOG_TAG, code.rawValue);
+                                break;
+                            case Barcode.SMS:
+                                Log.i(LOG_TAG, code.sms.message);
+                                break;
+                            case Barcode.TEXT:
+                                Log.i(LOG_TAG, code.rawValue);
+                                break;
+                            case Barcode.URL:
+                                Log.i(LOG_TAG, "url: " + code.url.url);
+                                break;
+                            case Barcode.WIFI:
+                                Log.i(LOG_TAG, code.wifi.ssid);
+                                break;
+                            case Barcode.GEO:
+                                Log.i(LOG_TAG, code.geoPoint.lat + ":" + code.geoPoint.lng);
+                                break;
+                            case Barcode.CALENDAR_EVENT:
+                                Log.i(LOG_TAG, code.calendarEvent.description);
+                                break;
+                            case Barcode.DRIVER_LICENSE:
+                                Log.i(LOG_TAG, code.driverLicense.licenseNumber);
+                                break;
+                            default:
+                                Log.i(LOG_TAG, code.rawValue);
+                                break;
+                        }
+                    }
+                    if (barcodes.size() == 0) {
+                        scanResults.setText("Scan Failed: Found nothing to scan");
+                    }
+                } else {
+                    scanResults.setText("Could not set up the detector!");
+                }
+            } catch (Exception e) {
+                Toast.makeText(this, "Failed to load Image", Toast.LENGTH_SHORT)
+                        .show();
+                Log.e(LOG_TAG, e.toString());
+            }
         }
-
-        String url_with_google_viewer = "http://docs.google.com/gview?embedded=true&url="+url;
-        wb.loadUrl(url_with_google_viewer );
     }
 
-    public String getUrlFromCode(BarcodeDetector detector, Bitmap qrCodeBitmap) {
-        Frame frame = new Frame.Builder().setBitmap(qrCodeBitmap).build();
-        SparseArray<Barcode> barcodes = detector.detect(frame);
+    private void takePicture() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        File photo = new File(Environment.getExternalStorageDirectory(), "picture.jpg");
+        imageUri = FileProvider.getUriForFile(ScanQRCodeActivity.this,
+                BuildConfig.APPLICATION_ID + ".provider", photo);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+        startActivityForResult(intent, PHOTO_REQUEST);
+    }
 
-        return barcodes.valueAt(0).rawValue;
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        if (imageUri != null) {
+            outState.putString(SAVED_INSTANCE_URI, imageUri.toString());
+            outState.putString(SAVED_INSTANCE_RESULT, scanResults.getText().toString());
+        }
+        super.onSaveInstanceState(outState);
+    }
+
+    private void launchMediaScanIntent() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        mediaScanIntent.setData(imageUri);
+        this.sendBroadcast(mediaScanIntent);
+    }
+
+    private Bitmap decodeBitmapUri(Context ctx, Uri uri) throws FileNotFoundException {
+        int targetW = 600;
+        int targetH = 600;
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeStream(ctx.getContentResolver().openInputStream(uri), null, bmOptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        int scaleFactor = Math.min(photoW / targetW, photoH / targetH);
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+
+        return BitmapFactory.decodeStream(ctx.getContentResolver()
+                .openInputStream(uri), null, bmOptions);
     }
 }
